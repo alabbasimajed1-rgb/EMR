59eff7d82f12f9484cc82c119214e96aea97b7df
alabbasimajed1-rgb <alabbasimajed1@gmail.com>
2026-07-13 02:50:37 +0300

    Update visit.dart

--- lib/models/visit.dart
+++ lib/models/visit.dart
@@ -1,47 +1,31 @@
 class Visit {
-  String? id;
-  String patientId;
-  DateTime visitDate;
-  String chiefComplaint;
-  String investigations;
-  String differentialDiagnosis;
-  String finalDiagnosis;
-  String treatmentPlan;
+  final String? id;
+  final String patientId;
+  final String procedure;
+  final DateTime visitDate;
 
   Visit({
     this.id,
     required this.patientId,
+    required this.procedure,
     required this.visitDate,
-    required this.chiefComplaint,
-    required this.investigations,
-    required this.differentialDiagnosis,
-    required this.finalDiagnosis,
-    required this.treatmentPlan,
   });
+
+  factory Visit.fromMap(Map<String, dynamic> map) {
+    return Visit(
+      id: map['id']?.toString(),
+      patientId: map['patientId'] ?? '',
+      procedure: map['procedure'] ?? '',
+      visitDate: DateTime.tryParse(map['visitDate'] ?? '') ?? DateTime.now(),
+    );
+  }
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'patientId': patientId,
+      'procedure': procedure,
       'visitDate': visitDate.toIso8601String(),
-      'chiefComplaint': chiefComplaint,
-      'investigations': investigations,
-      'differentialDiagnosis': differentialDiagnosis,
-      'finalDiagnosis': finalDiagnosis,
-      'treatmentPlan': treatmentPlan,
     };
-  }
-
-  factory Visit.fromMap(Map<String, dynamic> map) {
-    return Visit(
-      id: map['id'],
-      patientId: map['patientId'],
-      visitDate: DateTime.parse(map['visitDate']),
-      chiefComplaint: map['chiefComplaint'] ?? '',
-      investigations: map['investigations'] ?? '',
-      differentialDiagnosis: map['differentialDiagnosis'] ?? '',
-      finalDiagnosis: map['finalDiagnosis'] ?? '',
-      treatmentPlan: map['treatmentPlan'] ?? '',
-    );
   }
 }
