1c3b2441e39f34fc782fd20d1115acf4ac9b42ad
alabbasimajed1-rgb <alabbasimajed1@gmail.com>
2026-07-17 00:14:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/home_screen.dart'; // تأكد من اسم ملف الشاشة الرئيسية 
 import 'l10n/app_localizations.dart';
 
